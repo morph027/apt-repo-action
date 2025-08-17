dd09fefe4dacf761eb31589e5e2c3bb52b8c8a75
morph027 <stefan.heitmueller@gmx.com>
2025-08-17 15:53:13 +0200

    set GPG_TTY to fix 'Inappropriate ioctl for device'
    Signed-off-by: morph027 <stefan.heitmueller@gmx.com>

--- repo.sh
+++ repo.sh
@@ -33,6 +33,8 @@ keyring_files=()
 for fingerprint in "${fingerprints[@]}"; do
     keygrip="$(gpg --list-secret-keys --with-keygrip "${fingerprint}" | grep -m1 'Keygrip =' | grep -Eo "[0-9A-Z]{40}")"
     if [[ -n "${SIGNING_KEY_PASSPHRASE}" ]]; then
+        GPG_TTY="$(tty)"
+        export GPG_TTY
         pgrep -f bin/gpg-agent >/dev/null 2>&1 || gpg-agent --verbose --daemon --log-file /tmp/gpg-agent.log --allow-preset-passphrase
         /usr/lib/gnupg2/gpg-preset-passphrase --verbose --preset --passphrase "${SIGNING_KEY_PASSPHRASE}" "${keygrip}"
     fi
